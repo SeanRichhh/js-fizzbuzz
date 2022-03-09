241ac9ff4c9f9026c7cc0708197032295905e0b3
SeanRichhh <sean-richards@outlook.com>
2022-03-09 16:27:24 +0000

    1st test passed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,27 @@
 // TODO: Write your function in this file
+//Calling the function
+
+function fizzbuzz(lower,upper){
+    let result = []
+
+    for (let i = lower; i <= upper; i++){
+        result.push(i)
+        console.log(result);
+    }
+    
+
+
+return result
+}
+
+
+
+
+
 
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzbuzz
+
+
+//The Function should store an array which i have done on line 5 
+//
